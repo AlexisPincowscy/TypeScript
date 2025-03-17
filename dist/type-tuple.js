c39e86e53c6e1f6acb61adb48f36c2203215fcf0
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-17 14:42:39 -0300

    Aula de tuple types

---
+++ dist/type-tuple.js
@@ -0,0 +1,12 @@
+"use strict";
+// Tuples ou Tuplas - são arrays com tipos específicos
+// aqui faz diferença a posição do tipo de item do array, mais específico
+// arrays padrão
+let exeArray = [1, 'Guerra Civil', false]; // tudo certo aqui, ele aceita aquelas variações de valores, independtenda ordem
+// exemplo de tupla
+let exeTuple = [2, 'Cidade de Deus', true]; //aqui precisa estar na ordem certa do parâmetro, senão da erro
+// let exeTupleError: [number, string, boolean] = [false, 34, 'Carro'] // repare que aqui ele exibe erro de posicionamento
+// podemos dar opções de valores dentro de cada posição
+let exeTupleOption = [2, 'Cidade de Deus']; //aqui o ? deixa o valor ser o tipo ou não ter nada, caso exista a opção de não ser prenchido
+const [id, filmName, rented] = exeTuple; //aqui ele sabe exatemente qual tipo de valor será recebido na posição
+console.log(exeTuple);
