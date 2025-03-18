64365e03fd0cbdaba08af6c8de2b8072249cacc4
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-18 11:59:38 -0300

    Aula de let e conts no TS

---
+++ dist/let-vs-const.js
@@ -0,0 +1,11 @@
+"use strict";
+// Diferença entre let e const no TS
+Object.defineProperty(exports, "__esModule", { value: true });
+let phrase1 = 'Que a força esteja com você!';
+// quando digitamos o const e usamos var = valor, ela já o define como tipo literal
+const phrase2 = 'Como deve ser'; //veja ate´que muda de cor
+// agora, podemos tb transforamr uima let em const
+let phrase3 = 'Como tenho dito';
+// se tento modificiar esse valor ele vai reclamar
+// phrase3 = 'asd' não é possível
+exports.default = () => { };
--- dist/literal-types.js
+++ dist/literal-types.js
@@ -1,4 +1,5 @@
 "use strict";
+// Tipagem literal
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logger = logger;
 // aqui tipagem normal, tipo string e valor info
@@ -7,7 +8,6 @@ logType = 'warn';
 // ele aceita as mudanças
 // mas podemos fazer um tipo literal
 let logType2; //repare que não é = e sim :, então estou tipando ela com esse valor
-// logType2 = 'warn' // ela não aceita outro vlaor, a não ser info
 function logger(type, message) {
     switch (type) {
         case 'info':
@@ -21,4 +21,4 @@ function logger(type, message) {
             break;
     }
 }
-logger('error', '404 not found!');
+logger('error', '404 not found!'); //quando vamos chamar a função ela já deixa restrito as possibilidades, não aceitando um valor diferente
