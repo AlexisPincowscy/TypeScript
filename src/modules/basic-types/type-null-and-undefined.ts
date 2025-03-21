dff863aeddbe272f2d7ca0c977d5e0d0acaeefce
AlexisPincowscy <alexispincowscy@hotmail.com>
2025-03-21 11:44:24 -0300

    Aula de null e undefined

--- src/modules/basic-types/type-null-and-undefined.ts
+++ src/modules/basic-types/type-null-and-undefined.ts
@@ -1,3 +1,35 @@
 export const bootstrap = (): void => {
-    console.log("Null and undefined");
+
+    //null = valor em branco, null é um valor!!
+    console.log("Null");
+    
+    let title = null
+    console.log("Title:", title);
+    console.log('Title (if)', title ? 'verdadeiro':'falso');//false por padrão
+    console.log('Tipo null:', typeof(title));//bug histórico, retorna object
+    
+    //undefined = sem valor, variável vazia
+    console.log("\nUndefined");
+    
+    let subtitle = undefined
+    console.log("Subtitle:", subtitle);
+    console.log('Subtitle (if)', subtitle ? 'verdadeiro':'falso');//falso por padrão
+    console.log('Tipo null:', typeof(subtitle));
+
+
+    type Pages = {
+        title: string,
+        numberPage?: number, //pode ser number | undefined
+        orderPage?: () => string // pode ser string | undefined
+    }
+
+    let books: Pages = {
+        title : "Curso de TS"
+    }
+
+    console.log("Número de pags: ", books.numberPage);
+    console.log("Ordem das pags: ", books.orderPage);//aqui acessamos o valor, o () é o executar a função
+    // console.log("Ordem das pags: ", books.orderPage()) - aqui teríamos executado, nesse caso ele não deixa pq não pode
+    // executar função que po resultado será undefined
+    
 }
